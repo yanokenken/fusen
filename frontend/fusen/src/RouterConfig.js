81b620c5cd76912316bbb50f18cb3e4cf5976b5a
yanokenken <yanoken0822@gmail.com>
2023-11-07 01:49:28 +0900

    wip: Apply daisyUI

---
+++ frontend/fusen/src/RouterConfig.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+
+const RouterConfig = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<Home />} />
+
+			</Routes>
+		</BrowserRouter>
+	);
+}
+
+export default RouterConfig;
